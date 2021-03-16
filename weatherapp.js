ffb73a87a27877c37f7847ec9b2c6b12a269abfe
Jesse Woody <gearsighted@gmail.com>
2021-03-16 17:14:46 -0400

    security

--- weatherapp.js
+++ weatherapp.js
@@ -2,12 +2,12 @@ const form = document.querySelector('#citySearch')
 form.addEventListener('submit', async function (e){
     e.preventDefault();
     const userSearch = form.elements.query.value
-    const config = {params: {
-        q: userSearch, 
-        appid: '48200952b4f6813efdefdcdfc6490d97'
+    const res = await axios.get('pro.openweathermap.org/data/2.5/forecast/hourly', {
+        params: {
+            q: userSearch,
+            appid: "",
         }
-    }    
-    const res = await axios.get('pro.openweathermap.org/data/2.5/forecast/hourly', config)
+    })
     console.dir(res);
 })
 
