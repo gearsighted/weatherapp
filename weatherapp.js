b02e36a8ecd054f6b628d9b206a98aad74367dbb
Jesse Woody <gearsighted@gmail.com>
2021-03-16 15:32:54 -0400

    started functions, api key not working

--- weatherapp.js
+++ weatherapp.js
@@ -2,7 +2,13 @@ const form = document.querySelector('#citySearch')
 form.addEventListener('submit', async function (e){
     e.preventDefault();
     const userSearch = form.elements.query.value
-    const config = {params: {q: userSearch} {}}
+    const config = {params: {
+        q: userSearch, 
+        appid: '48200952b4f6813efdefdcdfc6490d97'
+        }
+    }    
+    const res = await axios.get('pro.openweathermap.org/data/2.5/forecast/hourly', config)
+    console.dir(res);
 })
 
 
