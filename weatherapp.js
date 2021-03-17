98906d7694fea268e25fde7494d2be4bbd3fe099
Jesse Woody <gearsighted@gmail.com>
2021-03-17 09:32:34 -0400

    retry commit

--- weatherapp.js
+++ weatherapp.js
@@ -1,10 +1,10 @@
-let config = config.config
+let params = config
 
 const form = document.querySelector('#citySearch')
 form.addEventListener('submit', async function (e){
     e.preventDefault();
     const userSearch = form.elements.query.value
-    const res = await axios.get('pro.openweathermap.org/data/2.5/forecast/hourly', config)
+    const res = await axios.get('pro.openweathermap.org/data/2.5/forecast/hourly', params)
     console.dir(res);
 })
 
