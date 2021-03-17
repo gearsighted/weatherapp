da036eb17480ea0716ea00b540bb3980c0e96590
Jesse Woody <gearsighted@gmail.com>
2021-03-17 11:20:25 -0400

    still failing with api call

--- weatherapp.js
+++ weatherapp.js
@@ -1,13 +1,14 @@
-let params = config
+let key = config.appid;
 
 const form = document.querySelector('#citySearch')
+
 form.addEventListener('submit', async function (e){
     e.preventDefault();
     const userSearch = form.elements.query.value
-    const res = await axios.get('pro.openweathermap.org/data/2.5/forecast/hourly', params)
+    const details =  {params: 
+        {q: userSearch,
+         appid: key,   
+        }}
+    const res = await axios.get('api.openweathermap.org/data/2.5/weather', details)
     console.dir(res);
-})
-
-
-
-// pro.openweathermap.org/data/2.5/forecast/hourly?q={city name}&appid={API key}
+});
