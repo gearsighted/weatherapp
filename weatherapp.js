57d1fa4575b0aff911b6a8e1ac26db480c280bfc
Jesse Woody <gearsighted@gmail.com>
2021-03-17 18:28:08 -0400

    parsed current weather data, added metric units, fixed spelling error

--- weatherapp.js
+++ weatherapp.js
@@ -7,11 +7,13 @@ form.addEventListener('submit', async function (e){
     const userSearch = form.elements.query.value
     const details =  {params: 
         {q: userSearch,
-         appid: key,   
+         appid: key,
+         units: 'metric',   
         }}
     try {
         const res = await axios.get('https://api.openweathermap.org/data/2.5/weather', details)
-    console.dir(res);
+        // console.dir(res);
+    console.log(res.data.main);
     } catch (e) {
         console.log(e);
     }   
