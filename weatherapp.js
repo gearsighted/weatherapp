a74c671fbe35161756ea77616f0ea8b687ef8ad8
Jesse Woody <gearsighted@gmail.com>
2021-03-17 08:24:59 -0400

    gitignore file created

--- weatherapp.js
+++ weatherapp.js
@@ -1,13 +1,10 @@
+let config = config.config
+
 const form = document.querySelector('#citySearch')
 form.addEventListener('submit', async function (e){
     e.preventDefault();
     const userSearch = form.elements.query.value
-    const res = await axios.get('pro.openweathermap.org/data/2.5/forecast/hourly', {
-        params: {
-            q: userSearch,
-            appid: "",
-        }
-    })
+    const res = await axios.get('pro.openweathermap.org/data/2.5/forecast/hourly', config)
     console.dir(res);
 })
 
