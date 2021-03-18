cd5d651338f98b89f4c1a1ba8472de9d9a8ea900
Jesse Woody <gearsighted@gmail.com>
2021-03-17 22:27:04 -0400

    working data and LI/Header system started

--- weatherapp.js
+++ weatherapp.js
@@ -1,6 +1,9 @@
 let key = config.appid;
 
-const form = document.querySelector('#citySearch')
+const form = document.querySelector('#citySearch');
+const list = document.querySelector('#weather');
+const cityHeader = document.querySelector('#cityHeader');
+const tempList = document.querySelector('#temp')
 
 form.addEventListener('submit', async function (e){
     e.preventDefault();
@@ -12,10 +15,25 @@ form.addEventListener('submit', async function (e){
         }}
     try {
         const res = await axios.get('https://api.openweathermap.org/data/2.5/weather', details)
-        // console.dir(res);
-    console.log(res.data.main);
+
+        const tempData= res.data.main.temp;
+        
+        cityHeader.innerText = `Here is the weather in ${userSearch}`
+        tempList.innerText = `The temperature is ${tempData} degrees celcius.`
+
+        console.log(res.data)
     } catch (e) {
         console.log(e);
     }   
     
 });
+
+
+
+const printWeatherList = () => {
+   
+}
+
+// .weather.0.description
+// .main.temperature
+// .main.temperature
