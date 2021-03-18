235e0c5e6c4c3cc03a7950396058dabef422067d
Jesse Woody <gearsighted@gmail.com>
2021-03-18 09:48:01 -0400

    variables for all current weather data. List system for display

--- weatherapp.js
+++ weatherapp.js
@@ -2,6 +2,8 @@ const key = config.appid;
 const form = document.querySelector('#citySearch');
 const cityHeader = document.querySelector('#cityHeader');
 const tempList = document.querySelector('#temp')
+const weatherList = document.querySelector('#weather')
+const humidityList = document.querySelector('#humidity')
 
 form.addEventListener('submit', async function (e){
     e.preventDefault();
@@ -15,9 +17,13 @@ form.addEventListener('submit', async function (e){
         const res = await axios.get('https://api.openweathermap.org/data/2.5/weather', details)
 
         const tempData= res.data.main.temp;
+        const weatherData = res.data.weather[0].description;
+        const humidityData = res.data.main.humidity;
         
         cityHeader.innerText = `Here is the weather in ${userSearch}`
         tempList.innerText = `The temperature is ${tempData} degrees celcius.`
+        weatherList.innerText = `The current condition is ${weatherData}.`
+        humidityList.innerText = `The current humdity is ${humidityData} percent.`
 
         console.log(res.data)
     } catch (e) {
@@ -25,13 +31,3 @@ form.addEventListener('submit', async function (e){
     }   
     
 });
-
-
-
-const printWeatherList = () => {
-   
-}
-
-// .weather.0.description
-// .main.temperature
-// .main.temperature
